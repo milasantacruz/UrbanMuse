d92e4038ed8230daa29c044c497660787d764b80
camila santacruz <milalovechaton@gmail.com>
2025-12-17 11:05:26 -0300

    Deploy: arreglar fallback SPA en GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1291bbbf03e93627db949f998d40d3fa
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "9d3740fb75bba9596ea70293a3c0a419",
+"flutter_bootstrap.js": "e7779c6c593e01f4b13457658ee66c5d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
